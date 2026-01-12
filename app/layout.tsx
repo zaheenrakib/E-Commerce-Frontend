cd6807d27d1954cc6521595748b8a749de001d10
zaheenrakib <rakibzaheen@gmail.com>
2026-01-12 10:51:19 +0600

    Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
       >
+        
         <Navbar />
         {children}
         <Footer />
